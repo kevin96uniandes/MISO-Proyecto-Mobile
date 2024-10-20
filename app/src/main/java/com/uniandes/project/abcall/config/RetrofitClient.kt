efaf97a72bf420714d529c89cc5cd665d7c37397
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-10-19 19:17:39 -0500

    Update ingress url

--- app/src/main/java/com/uniandes/project/abcall/config/RetrofitClient.kt
+++ app/src/main/java/com/uniandes/project/abcall/config/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private val BASE_URL = "http://192.168.18.14:3000"
+    private val BASE_URL = "http://34.111.136.182"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
