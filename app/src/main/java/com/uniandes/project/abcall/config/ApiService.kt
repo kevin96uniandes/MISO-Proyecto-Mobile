8b3164581179284410bc11c7254444ce13cb3c8a
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-10-24 22:46:40 -0500

    Add endpoint to api service

--- app/src/main/java/com/uniandes/project/abcall/config/ApiService.kt
+++ app/src/main/java/com/uniandes/project/abcall/config/ApiService.kt
@@ -2,10 +2,15 @@ package com.uniandes.project.abcall.config
 
 import com.uniandes.project.abcall.repositories.rest.AuthClient
 import com.uniandes.project.abcall.repositories.rest.RegisterUserClient
-
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+
 
 interface ApiService {
 
@@ -14,5 +19,10 @@ interface ApiService {
 
     @POST("user/register/user")
     fun register(@Body userRegisterRequestBody: RegisterUserClient.UserRegisterRequestBody): Call<RegisterUserClient.RegisterResponse>
+
+    @Multipart
+    @POST("/incident/create")
+    fun createIncidence(@Part body: RequestBody?, @Part files: List<MultipartBody.Part?>): Call<ResponseBody?>?
+
 
 }
