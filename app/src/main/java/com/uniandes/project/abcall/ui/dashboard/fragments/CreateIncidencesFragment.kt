d678da2f4f1fbcb3da1a6ce3a174875ad8cf37e7
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-10-26 18:13:49 -0500

    Add create incidences fragment

---
+++ app/src/main/java/com/uniandes/project/abcall/ui/dashboard/fragments/CreateIncidencesFragment.kt
@@ -0,0 +1,27 @@
+package com.uniandes.project.abcall.ui.dashboard.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uniandes.project.abcall.R
+
+
+class CreateIncidencesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_incidences, container, false)
+    }
+
+    companion object {
+        const val TITLE = "Crear incidencias"
+        @JvmStatic
+        fun newInstance() =
+            CreateIncidencesFragment()
+    }
+}
