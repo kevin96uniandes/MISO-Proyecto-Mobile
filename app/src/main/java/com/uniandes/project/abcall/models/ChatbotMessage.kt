e3252fb1c85cc5d8bd086a76e3adc1ff712ba703
Jorge A. Romero Gutiérrez <ja.romerog12@uniandes.edu.co>
2024-10-26 22:55:05 -0500

    Vista de chatbot

---
+++ app/src/main/java/com/uniandes/project/abcall/models/ChatbotMessage.kt
@@ -0,0 +1,7 @@
+package com.uniandes.project.abcall.models
+
+data class ChatbotMessage(
+    val text: String,
+    val time: String,
+    val isSent: Boolean
+)
---
+++ app/src/main/java/com/uniandes/project/abcall/ui/dashboard/fragments/IncidenceCreateChatbotFragment.kt
@@ -0,0 +1,82 @@
+package com.uniandes.project.abcall.ui.dashboard.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.uniandes.project.abcall.adapter.IncidentChatbotAdapter
+import com.uniandes.project.abcall.databinding.FragmentIncidenceCreateChatbotBinding
+import com.uniandes.project.abcall.models.ChatbotMessage
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class IncidenceCreateChatbotFragment : Fragment() {
+
+    private val steps: List<String> = listOf(
+        ""
+    )
+
+    private lateinit var messageAdapter: IncidentChatbotAdapter
+    private val messageList = mutableListOf<ChatbotMessage>()
+
+    private var _binding: FragmentIncidenceCreateChatbotBinding? = null
+    private val binding get() = _binding!!
+
+    private var messageIsSent: Boolean = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentIncidenceCreateChatbotBinding.inflate(inflater, container, false)
+
+        val recyclerView: RecyclerView = binding.recyclerChatbotViewMessages
+        messageAdapter = IncidentChatbotAdapter(messageList)
+        recyclerView.adapter = messageAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        val sendButton: ImageButton = binding.btnSendChatbot
+        val messageInput: EditText = binding.etMessageChatbot
+
+        sendButton.setOnClickListener {
+            val messageText = messageInput.text.toString().trim()
+            if (messageText.isNotEmpty()) {
+                addMessage(messageText, isSent = false)  // o isSent = false si es un mensaje recibido
+                messageInput.text.clear()
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun addMessage(text: String, isSent: Boolean) {
+        val message = ChatbotMessage(
+            text = text,
+            time = getCurrentTime(),
+            isSent = isSent
+        )
+        messageList.add(message)
+        messageAdapter.notifyItemInserted(messageList.size - 1)
+
+        binding.recyclerChatbotViewMessages.scrollToPosition(messageList.size - 1)
+    }
+
+    private fun getCurrentTime(): String {
+        val currentTime = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+        return formatter.format(currentTime)
+    }
+
+    companion object {
+        const val TITLE = "Chatbot"
+        @JvmStatic
+        fun newInstance() =
+            IncidenceCreateChatbotFragment()
+    }
+}
