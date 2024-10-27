78e6243d11108b08a64d9a88a38c892af7f46e27
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-10-26 19:16:54 -0500

    Add client create incidences fragment

--- app/src/main/java/com/uniandes/project/abcall/ui/dashboard/ui/createIncidences/CrateIncidencesFragment.kt
+++ app/src/main/java/com/uniandes/project/abcall/ui/dashboard/ui/createIncidences/CrateIncidencesFragment.kt
@@ -1,11 +1,15 @@
 package com.uniandes.project.abcall.ui.dashboard.ui.createIncidences
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.uniandes.project.abcall.IncidenceType
 import com.uniandes.project.abcall.databinding.FragmentCreateIncidencesBinding
 import com.uniandes.project.abcall.ui.dashboard.ui.createIncidences.CreateIncidencesViewModel
 
@@ -22,6 +26,7 @@ class CrateIncidencesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        var idIncidenceType = -1;
 
         val createIncidencesViewModel =
             ViewModelProvider(this).get(CreateIncidencesViewModel::class.java)
@@ -30,7 +35,26 @@ class CrateIncidencesFragment : Fragment() {
         val root: View = binding.root
 
 
+        val ilIncidenceType: TextInputLayout = binding.ilIncidenceType
+        val etIncidenceType: TextInputEditText = binding.etIncidenceType
 
+        etIncidenceType.setOnClickListener {
+            val items = IncidenceType.entries.map { "${it.id} - ${it.type}" }.toTypedArray()
+
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Selecciona una opción")
+                .setSingleChoiceItems(items, -1) { dialog, which ->
+                    val selectedItem = items[which]
+                    etIncidenceType.setText(selectedItem)
+                    idIncidenceType = selectedItem.split("-")[0].trim().toInt()
+                    //clearIdentificationTypeError()
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Cancelar") { dialog, which ->
+                    dialog.dismiss()
+                }
+            builder.show()
+        }
 
 
 
