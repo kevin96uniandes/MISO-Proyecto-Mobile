b115dcf3b00045440480811f7ac39521a47fea56
Victor Castaneda <v.castanedap@uniandes.edu.co>
2024-11-02 15:05:49 -0500

    Clean project

--- app/src/main/java/com/uniandes/project/abcall/ui/dashboard/ui/createIncidences/CreateIncidencesViewModel.kt
+++ app/src/main/java/com/uniandes/project/abcall/ui/dashboard/ui/createIncidences/CreateIncidencesViewModel.kt
@@ -1,21 +1,21 @@
 package com.uniandes.project.abcall.ui.dashboard.ui.createIncidences
 
 import androidx.lifecycle.ViewModel
-import com.uniandes.project.abcall.repositories.rest.CreateIncidence
+// import com.uniandes.project.abcall.repositories.rest.CreateIncidence
 import com.uniandes.project.abcall.repositories.rest.RegisterUserClient
 import okhttp3.MultipartBody
 
 
 class CreateIncidencesViewModel (
-    private val createIncidenceClient: CreateIncidence
+    // private val createIncidenceClient: CreateIncidence
     ) : ViewModel() {
 
-
+    /*
     fun createIncidence(type: Int, subject: String, detail: String, files: List<MultipartBody.Part>) {
         createIncidenceClient.createIncidence(type=type, channel = "mobil-app", subject = subject, detail = detail, files = files){ code ->
             RegisterUserClient.RegisterResponse(code)
         }
+    */
 
 
     }
-}
